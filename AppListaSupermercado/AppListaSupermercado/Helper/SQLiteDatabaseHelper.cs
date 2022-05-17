b09b9c944f44295a32f5955b6ed68a014f77ed99
Usuario <Usuario@CPD02-M00>
2022-05-17 11:53:25 -0300

    Adicionar arquivos de projeto.

---
+++ AppListaSupermercado/AppListaSupermercado/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+using AppListaSupermercado.Model;
+
+namespace AppListaSupermercado.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+        public void insert(Produto p)
+        {
+
+        }
+
+        public void getAllRows()
+        {
+
+        }
+
+        public void delete(int id)
+        {
+
+        }
+
+        public void update(Produto p)
+        {
+
+        }
+    }
+}
---
+++ AppListaSupermercado/AppListaSupermercado/Model/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+
+namespace AppListaSupermercado.Model
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public string Id { get; set; }
+        public string NomeProduto { get; set; }
+        public double Quantidade { get; set; }
+        public double PrecoEstimado { get; set; }
+        public double PrecoPago { get; set; }
+    }
+}
